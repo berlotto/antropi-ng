36a5dc4d53cd0e597961d866d7c92ca182c8ac12
Guilherme Guerra <guerrinha@comum.org>
2013-03-25 20:51:24 -0300

    Finalizando footer
    - Links para imagens e feed relativos

--- footer.php
+++ footer.php
@@ -10,30 +10,46 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-            <ul class="infos">
-            <li><img src="wp-content/themes/antropi-ng/ufgrs.png"></li>
-            <li>
-            <ul class="nucleo">
-            <li><h3>Antropologia da Propriedade Intelectual Departamento de Antropologia</h3></li>
-            <li>Instituto de Filosofia e Ciências Humanas</li>
-            <li>Prédio 43311 A – Sala 219</li>
-            <li>Campus Agronomia</li>
-            <li>Av. Bento Gonçalves 9500 - Porto Alegre - RS, 91501-970 </li>
-            <li>Telefone: (51) 3308-6865</li>
-            </ul></li>
-            <li>
-            <ul class="contact">
-            <li>feed de notícias (rss)</li>
-            <li>e-mail institucional:<br>
-             contato@antropi.org<li>
-            <li>assine nossa lista de discussão<br>
+
+</div><!-- #main .wrapper -->
+<footer id="colophon" role="contentinfo">
+  <ul class="infos">
+    <li class="ufrgs">
+      <a href="http://www.ufrgs.br">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/ufgrs.png">
+      </a>
+    </li>
+
+    <li>
+      <ul class="nucleo">
+        <li><h3>Antropologia da Propriedade Intelectual Departamento de Antropologia</h3></li>
+        <li>Instituto de Filosofia e Ciências Humanas</li>
+        <li>Prédio 43311 A – Sala 219</li>
+        <li>Campus Agronomia</li>
+        <li>Av. Bento Gonçalves 9500 - Porto Alegre - RS, 91501-970 </li>
+        <li>Telefone: (51) 3308-6865</li>
+      </ul></li>
+      <li>
+        <ul class="contact">
+          <li>
+            <a href="<?php bloginfo('rss2_url'); ?>">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/feed.png">
+            </a>
+          </li>
+          <li>
+            <b>e-mail institucional:</b><br>
+             <a href="mailto:contato@antropi.org">
+               contato@antropi.org
+            </a>
+          </li>
+          <li>
+            <b>assine nossa lista de discussão</b><br>
             antropi@ufrgs-listas.br
-            </li>
-            </ul></li>
-            </ul>
-	</footer><!-- #colophon -->
+          </li>
+        </ul>
+      </li>
+  </ul>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
